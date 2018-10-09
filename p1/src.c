234bc6cf751a8d1fda3b7024b99a01f889e75988
Michael Tang <michaeltang@Michaels-MacBook-Pro.local>
2018-10-09 01:21:14 -0500

    dljafl;ksdj

--- p1/src.c
+++ p1/src.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/* Server and client side code: get from RPI
+   different implementations for TCP and UDP */
 
 int main(int argc, char*argv[]){
-	int kek = 0;
-	printf("%d\n",kek);
+	if (argc < 2)
+		fprintf(stderr,"invalid or missing options\n");
+	for (int i=0;i<argc;i++)
+		printf("%s\n",argv[i]);
 	return 0;
 }
