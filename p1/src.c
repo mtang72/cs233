beef7a824b2815f8fa8a768fd0e215d6d12a520a
Michael Tang <michaeltang@Michaels-MacBook-Pro.local>
2018-10-09 16:40:47 -0500

    will i ever be able to proficiently code in c again

--- p1/src.c
+++ p1/src.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
 
 /* Server and client side code: get from RPI
    different implementations for TCP and UDP */
 
 int main(int argc, char*argv[]){
-	if (argc < 2)
+	int cliserv = 0; //0 if client 1 if server	
+	int tcpudp = 0; //0 if tcp 1 if udp
+	int port;
+	char hostnm[50];
+	for (int i=1;i<argc;i++){
+		if (strcmp(argv[i],"-l"))
+			cliserv = 1;
+		else if (strcmp(argv[i],"-u"))
+			tcpudp = 1;	
+		else{
+			if (i<argc-1){
+				for (int j=0;j<strlen(argv[i]);j++)
+					hostnm[j] = argv[i][j];
+			}
+			else
+				port = atoi(argv[i]);
+		}
+	}
+	printf("cliserv: %d\ntcpudp: %d\nport: %s\nhostnm: %s\n",
+		cliserv, tcpudp, port, hostnm);
+
+	if (argc<2 || cliserv){
 		fprintf(stderr,"invalid or missing options\n");
-	for (int i=0;i<argc;i++)
-		printf("%s\n",argv[i]);
+		fprintf(stderr,"usage: snc [-l] [-u] [hostname] port\n");
+	}
 	return 0;
 }
