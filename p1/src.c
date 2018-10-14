4534f6b1ce8f8bbfa522a9227ea4aef45e584ac3
Michael Tang <michaeltang@Michaels-MacBook-Pro.local>
2018-10-13 21:43:39 -0500

    final?

--- p1/src.c
+++ p1/src.c
@@ -27,10 +27,10 @@ void server(int sockfd,char* hostnm,int port,int is_udp){
 		serv_addr.sin_addr.s_addr = INADDR_ANY;
 	else{
 		if (inet_pton(AF_INET,hostnm,&serv_addr.sin_addr.s_addr)==0)
-			error("ERROR invalid address");
+			error("internal error");
 	}
 	if (bind(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0)
-		error("binding error");
+		error("internal error");
 	//listening and connecting to client if TCP	
 	listen(sockfd, 5);
 	socklen_t clilen = sizeof(cli_addr);
@@ -38,7 +38,7 @@ void server(int sockfd,char* hostnm,int port,int is_udp){
 	if (!is_udp){
 		newsockfd = accept(sockfd,(struct sockaddr*)&cli_addr, &clilen);
 		if (newsockfd<0)
-			error("accept error");
+			error("internal error");
 	}
 	//receiving messages
 	while(1){
@@ -61,7 +61,7 @@ void client(int sockfd,char* hostnm,int port,int is_udp){
 	char buffer[256];
 	server = gethostbyname(hostnm);
 	if (server == NULL)
-		error("No such host");
+		error("invalid or missing options\nusage: snc [-l] [-u] [hostname] port");
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,server->h_length);
@@ -69,7 +69,7 @@ void client(int sockfd,char* hostnm,int port,int is_udp){
 	//attempting to connect to server
 	if (!is_udp){
 		if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-			error("ERROR connecting");
+			error("internal error");
 	}
 	int n, can_send = 1;
 	//messaging
@@ -91,7 +91,7 @@ void client(int sockfd,char* hostnm,int port,int is_udp){
 		/*bzero(buffer,256);
 		n = read(sockfd,buffer,255);
 		if (n < 0) 
-			error("ERROR reading from socket");
+			error("internal error");
 		printf("%s\n",buffer);*/
 	}
 }
@@ -113,7 +113,8 @@ int main(int argc, char*argv[]){
 				port = atoi(argv[i]);
 		}
 	}
-	if (argc<2 || (!is_serv&&!strcmp(hostnm,"none")) || port==0)
+	if (argc<2 || (!is_serv&&!strcmp(hostnm,"none")) || port<1025
+		|| port>65535)
 		error("invalid or missing options\nusage: snc [-l] [-u] [hostname] port");
 	//opening socket
 	int sockfd;
@@ -122,13 +123,13 @@ int main(int argc, char*argv[]){
 	else
 		sockfd = socket(AF_INET, SOCK_STREAM, 0);
     	if (sockfd < 0) 
-        	error("ERROR opening socket");	
+        	error("internal error");	
 	
 	if (is_serv)
 		server(sockfd,hostnm,port,is_udp);
 	else
 		client(sockfd,hostnm,port,is_udp);
 	if (close(sockfd)<0)
-		error("ERROR closing socket");
+		error("internal error");
 	return 0;
 }
