fea86a9d15a838eccd27c71743ba7b1dba52b7a5
Michael Tang <michaeltang@Michaels-MacBook-Pro.local>
2018-11-16 12:16:34 -0600

    successful completion of part 1 barring edge case testing

--- p2/pftp.py
+++ p2/pftp.py
@@ -1,21 +1,52 @@
 #i'm sorry to all the fans but i would seriously rather use sublime text than vim
-import argparse,sys,socket
+import argparse,sys,socket,re
+class ConnectionError(Exception):
+	pass
 
-def ftp_nonpar(file,hostnm,user,pwd,port,out=sys.stdout):
+def ftp_nonpar(file,hostnm,user,pwd,port,out):
 	addr = socket.gethostbyname(hostnm)
 	#soc.bind(('',port))
-	with socket.socket() as soc:
-		soc.connect((hostnm,port))
-		count = 0
-		while True:
-			count+=1
-			print("loop ",count)
-			soc.sendall(input().encode())
-			reply = soc.recv(256)
-			if not reply:
-				break
-			print(reply.decode())
-
+	soc1 = socket.socket()
+	soc2 = socket.socket()
+	soc1.connect((hostnm,port))
+	while True:
+		rec = soc1.recv(256)
+		if not rec:
+			break
+		msg = rec.decode().strip()
+		code,snd = msg[0:3], "" #to get the message code
+		if code=="220": #username ask
+			snd = "USER "+user
+		if code=="331": #password ask
+			snd = "PASS "+pwd
+		if code=="430" or code=="530": #invalid username/pass
+			raise ValueError("invalid username/password")
+		if code=="550": #invalid filename
+			raise ValueError("invalid filename")
+		if code=="421": #timeout
+			raise ConnectionError()
+		if code=="150": #file sending
+			with open(file, "wb") as f:
+				while True:
+					chunk = soc2.recv(2048)
+					if not chunk:
+						break
+					f.write(chunk)
+			snd = "QUIT"
+		if code=="230" or code=="425": #login successful/select PASV
+			snd = "PASV"
+		if code=="227": #PASV successful
+			hostaddr = ".".join(re.search(r"\(\d{1,3},\d{1,3},\d{1,3},\d{1,3}",msg).group()[1:].split(","))
+			p1, p2 = map(int,re.search(r"\d{0,5},\d{0,5}\)",msg).group()[:-1].split(","))
+			soc2.connect((hostaddr, p1*256+p2))
+			snd = "RETR "+file
+		data = (snd+"\n").encode()
+		if out:
+			print("S->C: {}\nC->S: {}".format(msg,snd), file=out)
+		soc1.sendall(data)
+	soc1.close()
+	soc2.close()
+	return "File transfer successful."
 
 
 if __name__ == "__main__":
@@ -26,12 +57,14 @@ if __name__ == "__main__":
 	parser.add_argument("-p","--port",type=int,metavar="port",default=21)
 	parser.add_argument("-n","--username",metavar="user",default="anonymous")
 	parser.add_argument("-P","--password",metavar="password",default="user@localhost.localnet")
-	parser.add_argument("-l","--log",metavar="logfile")
+	parser.add_argument("-l","--log",metavar="logfile",default=None)
 	args = vars(parser.parse_args())
 	file = args['file']
 	hostnm = args['hostname']
-	log = args['log']
+	log = open(args['log'],'w') if args['log']!='-' else (sys.stdout if args['log']=='-' else None)
 	pwd = args['password']
 	port = args['port']
 	user = args['username']
-	ftp_nonpar(file,hostnm,user,pwd,port)
+	print(ftp_nonpar(file,hostnm,user,pwd,port,log))
+	if log!=sys.stdout:
+		log.close()
