c840fcdee289a606b5f1890f0c4df232012511dc
Michael Tang <michaeltang@Michaels-MacBook-Pro.local>
2018-11-19 00:12:55 -0600

    DONE

--- p2/pftp.py
+++ p2/pftp.py
@@ -18,23 +18,32 @@ import argparse,sys,socket,re,threading,os,tempfile
 	raise Exception("7: Generic error")"""
 
 
-def ftp(file,hostnm,user,pwd,port,out,inter=None,posn=None): #inter,pos should be (#intervals, position) when it exists
+def ftp(filenm,file,hostnm,user,pwd,port,out,inter=None,posn=None,results=None): 
+#inter,pos should be (#intervals, position) when it exists
+#results is a list to document the amount of threads that successfully return
 	addr = socket.gethostbyname(hostnm)
 	#soc.bind(('',port))
 	soc1 = socket.socket()
 	soc2 = socket.socket()
+	soc1.settimeout(60)
+	soc2.settimeout(60)
 	soc1.connect((hostnm,port))
 	filesize = 0
 	while True:
-		rec = soc1.recv(256)
-		if not rec:
+		try:
+			rec = soc1.recv(256)
+		except socket.timeout:
+			raise Exception("1: Can't connect to server")
+		if not rec: #just in case
 			break
 		msg = rec.decode().strip()
 		if out:
-			print(("T{}: ".format(posn) if posn!=None else "")+"S->C: ",msg, file=out)
+			print(("T{}: ".format(posn+1) if posn!=None else "")+"S->C: ",msg, file=out)
 		code,snd = msg[0:3], "" #to get the message code
+		if code=="221": #goodbye
+			break
 		if code=="500": #incorrect command
-			raise Exception("5: Command not implemented by server")
+			raise Exception("5: Command not implemented/Operation not allowed")
 		if code=="501":
 			raise Exception("4: Syntax error in client request")
 		if code=="220": #username ask
@@ -45,24 +54,29 @@ def ftp(file,hostnm,user,pwd,port,out,inter=None,posn=None): #inter,pos should b
 			raise Exception("2: Authentication failed")
 		if code=="550": #invalid filename
 			raise Exception("3: File not found")
-		if code=="421": #timeout
+		if code=="421" or code=="426": #timeout
 			raise Exception("1: Can't connect to server")
 		if code=="150": #file sending
 			filesize //= inter if inter else 1
-			with open(file, "wb") as f:
-				pos = 0
-				while pos<filesize:
+			pos = 0
+			while pos<filesize:
+				try:
 					chunk = soc2.recv(min(filesize-pos,4096))
-					pos += len(chunk)
-					#print("pos: {}  last packet size: {}".format(pos,len(chunk)))
-					if not chunk:
-						break
-					f.write(chunk)
-				while (posn!=None and inter!=None) and posn==inter-1: #reading leftover bytes
+				except socket.timeout:
+					raise Exception("1: Can't connect to server")
+				pos += len(chunk)
+				#print("pos: {}  last packet size: {}".format(pos,len(chunk)))
+				if not chunk:
+					break
+				file.write(chunk)
+			while (posn!=None and inter!=None) and posn==inter-1: #reading leftover bytes
+				try:
 					chunk = soc2.recv(1024)
-					if not chunk:
-						break
-					f.write(chunk)
+				except socket.timeout:
+					raise Exception("1: Can't connect to server")
+				if not chunk:
+					break
+				file.write(chunk)
 		if code=="226": #transfer complete
 			snd = "QUIT"
 		if code=="230" or code=="425": #login successful/select PASV
@@ -71,7 +85,7 @@ def ftp(file,hostnm,user,pwd,port,out,inter=None,posn=None): #inter,pos should b
 			hostaddr = ".".join(re.search(r"\(\d{1,3},\d{1,3},\d{1,3},\d{1,3}",msg).group()[1:].split(","))
 			p1, p2 = map(int,re.search(r"\d{0,5},\d{0,5}\)",msg).group()[:-1].split(","))
 			soc2.connect((hostaddr, p1*256+p2))
-			snd = "SIZE "+file
+			snd = "SIZE "+filenm
 		if code=="213": #size of file received
 			filesize = int(msg.split()[1])
 			snd = "TYPE I"
@@ -79,46 +93,46 @@ def ftp(file,hostnm,user,pwd,port,out,inter=None,posn=None): #inter,pos should b
 			pos = (filesize//inter)*posn if inter else 0
 			snd = "REST {}".format(pos)
 		if code=="350": #restart position accepted
-			snd = "RETR "+file
+			snd = "RETR "+filenm
 		data = (snd+"\n").encode()
 		if out:
-			print(("T{}: ".format(posn) if posn!=None else "")+"C->S: ",snd, file=out)
-		soc1.sendall(data)
+			print(("T{}: ".format(posn+1) if posn!=None else "")+"C->S: ",snd, file=out)
+		try:
+			soc1.sendall(data)
+		except socket.timeout:
+			raise Exception("1: Can't connect to server")
 	soc1.close()
 	soc2.close()
+	if results!=None:
+		results.append(0)
 	return 0
 
 def threadft(cmds, port, log): #multithread downloading
-	threads = []
+	threads, tmpfiles, results = [], [], []
 	for i in range(len(cmds)):
 		user, pwd = re.search(r"(?<=ftp://).*:.*(?=@)",cmds[i]).group().split(":")
 		hostnm = re.search(r"(?<=@).*(?=/)",cmds[i]).group()
-		file = re.search(r"/.*$",cmds[i][6:]).group()[1:]
-		threads.append(threading.Thread(target=ftp, args=(file,hostnm,user,pwd,port,log),kwargs={'inter':len(cmds),'posn':i}))
+		filenm = re.search(r"/.*$",cmds[i][6:]).group()[1:]
+		tmpfiles.append(tempfile.TemporaryFile())
+		threads.append(threading.Thread(target=ftp, args=(filenm,tmpfiles[i],hostnm,user,pwd,port,log),\
+			kwargs={'inter':len(cmds),'posn':i,'results':results}))
 	for thread in threads:
 		thread.start()
 	for thread in threads:
 		thread.join()
-	#file = str(posn)+"-"+file
-	with open("0-"+file,"ab") as f:
-		for i in range(1,len(cmds)):
-			try:
-				with open(str(i)+"-"+file,"rb") as f1:
-					f.write(f1.read())
-				os.remove(str(i)+"-"+file)
-			except FileNotFoundError:
-				err(file, log, len(cmds))
-		try:
-			os.remove(file)
-		except FileNotFoundError:
-			pass
-		os.rename("0-"+file,file)
+	if len(results) != len(threads):
+		raise Exception("6: Thread generic failure")
+	with open(filenm,"wb") as f:
+		for tmpfile in tmpfiles:
+			tmpfile.seek(0)
+			f.write(tmpfile.read())
+			tmpfile.close()
 	return 0
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(prog="pftp")
 	parser.add_argument("-s",metavar="hostname",dest="hostname")
-	parser.add_argument("-f",metavar="file",dest="file")
+	parser.add_argument("-f",metavar="file",dest="filenm")
 	parser.add_argument("-v", "--version",action="version",version="%(prog)s v0.1 by Michael Tang (mtang72)")
 	parser.add_argument("-p","--port",type=int,metavar="port",default=21)
 	parser.add_argument("-n","--username",metavar="user",default="anonymous")
@@ -126,9 +140,12 @@ if __name__ == "__main__":
 	parser.add_argument("-l","--log",metavar="logfile")
 	parser.add_argument("-t","--thread",metavar="cfg_file",dest="cfg")
 	args = vars(parser.parse_args())
-	if not args['cfg'] and not (args['hostname'] or args['file']):
+	if not args['cfg'] and not (args['hostname'] or args['filenm']):
+		if len(sys.argv) == 1:
+			parser.print_help()
+			sys.exit()
 		parser.error("-s, -f required if -t not given")
-	file = args['file']
+	filenm = args['filenm']
 	hostnm = args['hostname']
 	if args['log']:
 		log = open(args['log'],'w') if args['log']!='-' else sys.stdout
@@ -140,16 +157,14 @@ if __name__ == "__main__":
 	if args['cfg']: #read config file, override parameters and enter multithread mode
 		with open(args['cfg'],'r') as f:
 			cmds = f.read().strip().split('\n')
-			try:
-				if threadft(cmds, port, log) == 0:
-					print("0: Operation successfully completed")
-			except KeyboardInterrupt:
-				err(file,log,len(cmds))
-	else:
-		try:
-			if ftp(file,hostnm,user,pwd,port,log) == 0:
+			if threadft(cmds, port, log) == 0:
 				print("0: Operation successfully completed")
-		except KeyboardInterrupt:
-			err(file,log,None)
+	else:
+		with tempfile.TemporaryFile() as file:
+			if ftp(filenm,file,hostnm,user,pwd,port,log) == 0:
+				with open(filenm,"wb") as f:
+					file.seek(0)
+					f.write(file.read())
+				print("0: Operation successfully completed")
 	if log and log!=sys.stdout:
 		log.close()
