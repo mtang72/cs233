6dd322c71c7149c82d6caa0798d4de15643e7f3b
Michael Tang <michaeltang@Michaels-MacBook-Pro.local>
2018-12-11 12:06:03 -0600

    finally able to do without header but being rate limited. maybe need to implement cookie

--- p3/mcrawl.py
+++ p3/mcrawl.py
@@ -9,61 +9,27 @@ def webcrawl(hostnm,port,files):
 		raise Exception("FUCK")
 	links = ['/index.html']
 	while links!=[]:
-
 		#get header
-		cmd = 'HEAD {} HTTP/1.1\r\nHost: {}\r\n\r\n'.format(links[-1],hostnm)
+		cmd = 'GET {} HTTP/1.1\r\nHost: {}\r\n\r\n'.format(links[-1],hostnm)
 		try:
 			soc.sendall(cmd.encode())
 		except:
 			raise Exception('fuck')
-		header = ''
+		header = b''
 		while True:
-			rec = soc.recv(2048)
-			header += rec.decode()
-			if rec[-4:] == b'\r\n\r\n':
+			rec = soc.recv(512)
+			header += rec
+			if b'\r\n\r\n' in header:
 				break
-		
-		#is this an incorrect fucking filename? FUCK
-		if '404' in header.split('\n')[0]:
-			print("FUCK YOU ",links[-1])
-			links.pop()
-			continue
 
-		#did they fucking MOVE IT OH MY GOD
-		if '301' in header.split('\n')[0]:
-			newloc = re.search(r'(?<=Location:).*',header).group().strip()
-			links.pop()
-			links.append(newloc)
-			continue
-
-		#are they rate limiting me? ITS TIME TO FUCKING DIE
-		#if 
-
-		#obviously, to determine if it's chonkytime or not
-		encoding_type = re.search(r'(?<=Transfer-Encoding:).*',header,re.IGNORECASE)
-		chonkytime = encoding_type!=None and re.match(r'chunked',encoding_type.group().strip(),re.IGNORECASE)!=None
-
-		#now get
-		cmd = 'GET'+cmd[4:]
-		try:
-			soc.sendall(cmd.encode())
-		except:
-			raise Exception('fuck')
-
-		#skip header to start read
-		pos = 0
-		while pos<len(header):
-			rec = soc.recv(len(header))
-			pos += len(rec)
-
-		#open file to write in and dir to attach to given links
+		#file handling
+		#specify filename to write in and dir to attach to given links
 		filenm, dirnm = '',''
 		if '/' in links[-1]:
 			filenm = links[-1].split('/')[-1]
 			dirnm = '/'.join(links[-1].split('/')[:-1])
 		else:
 			filenm = links[-1].split('/')[-1] if '/' in links[-1] else links[-1]
-
 		#handle duplicate files
 		dup = 1
 		filenm1 = filenm
@@ -75,26 +41,52 @@ def webcrawl(hostnm,port,files):
 			else:
 				filenm1 += '-'+str(dup)
 			dup+=1
+		#open file
 		filenm = filenm1
 		files[filenm] = tempfile.TemporaryFile()
 		f = files[filenm]
+		#pick out header and write initial parts of file leftover from header read
+		head_end = header.index(b'\r\n\r\n')
+		post_head = header[head_end+4:]
+		f.write(post_head)
+		header = header[:head_end].decode()
 
+		#obviously, to determine if it's chonkytime or not
+		encoding_type = re.search(r'(?<=Transfer-Encoding:).*',header,re.IGNORECASE)
+		chonkytime = encoding_type!=None and re.match(r'chunked',encoding_type.group().strip(),re.IGNORECASE)!=None
 		if not chonkytime:
-			sz = int(re.search(r'(?<=Content-Length:).*',header,re.IGNORECASE).group().strip())
-
+			clength = re.search(r'(?<=Content-Length:).*',header,re.IGNORECASE)
+			sz = int(clength.group().strip())-len(post_head) if clength else False
 		#actually reading the file
 		while True: 
-			sz = '' if chonkytime else sz
-			while chonkytime:
-				chonk = soc.recv(1)
-				if chonk == b'\n':
-					sz = int(sz[:-1],base=16)+2
-					break
-				sz += chonk.decode()
+			sz = b'' if chonkytime else sz
+			if chonkytime:
+				if post_head!=b'':
+					sz_end = post_head.index(b'\r')
+					sz += post_head[:sz_end]
+					post_head = post_head[sz_end+2:]
+					sz = int(sz,base=16)+2
+				if type(sz)!=int:
+					while True:
+						chonk = soc.recv(1)
+						sz += chonk
+						if sz[-1:] == b'\n':
+							sz = int(sz[:-2],base=16)+2
+							break
+				elif post_head!=b'':
+					sz -= len(post_head)
+					post_head = b''
 			if chonkytime and sz==2:
 				break
 			pos = 0
-			while pos<sz:
+			if not sz:
+				post_head_complete = not re.search(rb'</html>',post_head)
+				while post_head_complete:
+					chonk = soc.recv(2048)
+					if re.search(rb'</html>',chonk):
+						break
+					f.write(chonk)
+			while sz and pos<sz:
 				chonk = soc.recv(min(2048,sz-pos))
 				pos+=len(chonk)
 				f.write(chonk if chonk[-2:]!='\r\n' else chonk[:-2]) #remove the extra \r\n from the end of the chunk
@@ -109,9 +101,31 @@ def webcrawl(hostnm,port,files):
 		links.pop()
 		f.seek(0)
 
+		#eRRORs
+		#is this an incorrect fucking filename? FUCK
+		if '404' in header.split('\n')[0]:
+			print("FUCK YOU ",links[-1])
+			f.close()
+			del files[filenm]
+			continue
+		#did they fucking MOVE IT OH MY GOD
+		if '301' in header.split('\n')[0]:
+			newloc = re.search(r'(?<=Location:).*',header).group().strip()
+			links.append(newloc)
+			f.close()
+			del files[filenm]
+			continue
+		#am i retarded yeah probably
+		if '400' in header.split('\n')[0]:
+			raise Exception('malformed command: {}'.format(cmd))
+		#are they rate limiting me? ITS TIME TO FUCKING DIE
+		if re.search(r'Connection: close',header,re.IGNORECASE):
+			return 1
+			#raise Exception("i'm being fucking rate limited ree")
+
 		#parsing links
-		del_crap = lambda x:x.strip()!='' and '#' not in x\
-			and not (('http://' in x or 'https://' in x) and re.search(r'(?<=://)[^/]*',x).group()!=hostnm)
+		del_crap = lambda x:x and '#' not in x and not (('http://' in x or 'https://' in x)\
+			and re.search(r'(?<=://)[^/]*',x).group()!=hostnm)
 		try:
 			txt = f.read().decode()
 		except UnicodeDecodeError:
@@ -142,8 +156,10 @@ if __name__ == '__main__':
 	if args['nthreads'] > 1:
 		multithread()
 	files = {}
-	webcrawl(args['hostnm'],args['port'],files)
+	print(webcrawl(args['hostnm'],args['port'],files))
 	for file in files.keys():
+		if file=='':
+			continue
 		with open(file,'wb') as f:
 			f.write(files[file].read())
 		files[file].close()
